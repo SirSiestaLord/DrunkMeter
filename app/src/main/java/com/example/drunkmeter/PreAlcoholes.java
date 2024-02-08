c14a0d387a6f34133785313632f37f825d7d0f9c
Tuğcan Çağlayan <tugcancaglayan@gmail.com>
2024-02-08 23:21:14 +0300

    updates

--- app/src/main/java/com/example/drunkmeter/PreAlcoholes.java
+++ app/src/main/java/com/example/drunkmeter/PreAlcoholes.java
@@ -6,16 +6,30 @@ public class PreAlcoholes {
     public void AutomaticCreator(Context context){
 
         SQL2 sql=new SQL2(context);
-        sql.AddPREAlcohol("Bira","Maltepe","Beer",9,200);
-        sql.AddPREAlcohol("Bira","Maltepe","Beer",9,800);
-        sql.AddPREAlcohol("Bira","Maltepe","Beer",15,800);
+        //DeleteallPres(context.getApplicationContext());
+        sql.AddPREAlcohol("Beer","Example Brand","Beer",5,200);
+        sql.AddPREAlcohol("Beer","Example Brand","Beer",5,800);
+        sql.AddPREAlcohol("Beer","Example Brand","Beer",5,800);
+        sql.AddPREAlcohol("Wine","Example Brand","Wine",14,200);
+        sql.AddPREAlcohol("Wine","Example Brand","Wine",14,800);
+        sql.AddPREAlcohol("Wine","Example Brand","Wine",14,800);
+        sql.AddPREAlcohol("Scotch","Example Brand","Scotch",50,200);
+        sql.AddPREAlcohol("Scotch","Example Brand","Scotch",50,800);
+        sql.AddPREAlcohol("Scotch","Example Brand","Scotch",50,800);
 
-       // DeleteallPres(context.getApplicationContext());
+        sql.AddPREAlcohol("Votka","Example Brand","Votka",40,200);
+        sql.AddPREAlcohol("Votka","Example Brand","Votka",40,800);
+        sql.AddPREAlcohol("Votka","Example Brand","Votka",40,800);
+
+        sql.AddPREAlcohol("Raki","Example Brand","Raki",47,200);
+        sql.AddPREAlcohol("Raki","Example Brand","Raki",47,800);
+        sql.AddPREAlcohol("Raki","Example Brand","Raki",47,800);
+
     }
     public void DeleteallPres(Context context){
         SQL2 sql=new SQL2(context);
 
-        for (int i =0;i<20;i++){
+        for (int i =0;i<50;i++){
             sql.DeletePREAlcohol(i);
         }
     }
